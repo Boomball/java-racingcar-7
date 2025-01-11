249187a087173cea7c9027cae460bff7bba5403e
kangrae <kangrae0501@gmail.com>
2025-01-11 17:58:02 +0900

    feat: 한 라운드 동안 자동차를 움직이는 기능 구현

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -5,14 +5,27 @@ import java.util.List;
 public class Race {
 
     private final Cars cars;
+    private int round;
 
-    public Race(List<String> names) {
+    public Race(List<String> names, int round) {
+        validate(round);
+
         List<Car> cars = names.stream().map(Car::new).toList();
         this.cars = new Cars(cars);
+        this.round = round;
     }
 
-    public void playRound() {
-        //int randomNumber = Randoms.pickNumberInRange(0, 9);
+    public int playRound() {
+        cars.move();
+        return --round;
+    }
 
+    private void validate(int round) {
+        if (round <= 0) {
+            throw new IllegalArgumentException("라운드는 0보다 작을 수 없습니다.");
+        }
+        if (round == Integer.MAX_VALUE) {
+            throw new IllegalArgumentException("라운드는 MAX_VALUE보다 클 수 없습니다.");
+        }
     }
 }

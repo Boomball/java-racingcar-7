7eb1af40be0acaeb49dfe089de62bc04a40426e6
kangrae <kangrae0501@gmail.com>
2025-01-11 17:31:15 +0900

    feat: 자동차 리스트를 다루는 객체 추가

---
+++ src/main/java/racingcar/Cars.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+import java.util.List;
+
+public class Cars {
+
+    List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    // 우승자 판별
+
+
+}

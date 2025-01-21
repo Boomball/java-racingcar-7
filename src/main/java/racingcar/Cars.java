d4da7fcc2431fc3e7bc413be1cf38d694985d3ea
kangrae <kangrae0501@gmail.com>
2025-01-21 15:15:09 +0900

    feat: 진짜 모든 구현 완료

--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -3,12 +3,14 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cars {
 
     private final List<Car> cars;
 
     public Cars(List<Car> cars) {
+        validate(cars);
         this.cars = cars;
     }
 
@@ -37,5 +39,15 @@ public class Cars {
     public String toString() {
         String regex = "\\[|\\]";
         return cars.toString().replaceAll(regex, "");
+    }
+
+    private static void validate(List<Car> cars) {
+        int size = cars.stream()
+                .map(Car::toString)
+                .collect(Collectors.toSet())
+                .size();
+        if (cars.size() != size) {
+            throw new IllegalArgumentException("[ERROR]: 자동차 명은 중복될 수 없습니다.");
+        }
     }
 }
